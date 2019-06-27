0913ed108d82b78052cdbcfea1bed5faae7c97f9
MaksymBobryshev <kcmame@gmail.com>
2019-06-27 18:07:15 +0300

    added some improvments

--- Board.rb
+++ Board.rb
@@ -1,13 +1,45 @@
+require_relative 'Robot'
 class Board
-  attr_reader x
-  attr_reader y
+  attr_accessor :rows, :columns
 
-  def Print
-
+  def start
+    puts 'Enter command'
+    loop do
+      command = gets.chop
+      if @robot.valid_command?(command)
+        @robot.execute_command(command, @rows, @columns)
+      else
+        puts 'Wrong command'
+      end
+    end
   end
+
   def initialize(x = 5, y = 6)
-    @x = x
-    @x = y
+    fill_the_matrix(x, y)
     @robot = Robot.new
   end
+
+  private
+
+  def fill_the_matrix(x, y)
+    @rows = Array.new(x, 0)
+    @columns = Array.new(y, 0)
+  end
 end
+
+
+
+Board.new().start
+
+
+
+
+
+
+=begin
+[4][0]  [4][1]  [4][2]  [4][3]  [4][4]  [4][5]          [0][0]  [0][1]  [0][2]  [0][3]  [0][4]  [0][5]
+[3][0]  [3][1]  [3][2]  [3][3]  [3][4]  [3][5]          [1][0]  [1][1]  [1][2]  [1][3]  [1][4]  [1][5]
+[2][0]  [2][1]  [2][2]  [2][3]  [2][4]  [2][5]          [2][0]  [2][1]  [2][2]  [2][3]  [2][4]  [2][5]
+[1][0]  [1][1]  [1][2]  [1][3]  [1][4]  [1][5]          [3][0]  [3][1]  [3][2]  [3][3]  [3][4]  [3][5]
+[0][0]  [0][1]  [0][2]  [0][3]  [0][4]  [0][5]          [4][0]  [4][1]  [4][2]  [4][3]  [4][4]  [4][5]
+=end
--- Robot.rb
+++ Robot.rb
@@ -1,49 +1,127 @@
 class Robot
-  attr_accessor x
-  attr_accessor y
-  @placed = false
-  public
-  def Execute_command(command)
+  @placed = nil
+public
+  def execute_command(command, rows, columns)
     case command.downcase
     when 'move'
-      Move
+      move(rows, columns)
     when 'left'
-      Left
+      left
     when 'right'
-      Right
+      right
     when 'report'
-      Report
+      report(rows, columns)
     else
-      args = command.split
-      Place(args[1].to_i, args[2].to_i, args[3].to_i)
+      arr = command.split       #=> ['place' 'x,y,direction']
+      args = arr[1].split(',')  #=> ['x', 'y', 'direction']
+      place(args[0].to_i, args[1].to_i, args[2], rows, columns)
     end
   end
-  private
-  def can_move?
 
-  end
-
-  def Place(x, y, f)
-    @x = x
-    @y = y
-    @f = f
-    @placed = true
-  end
-  def Move
-    if @placed
-      if can_move?
-
+  def valid_command?(command)
+    case command.downcase
+    when 'move', 'left', 'right', 'report'
+      return true
+    else
+      command_array = command.downcase.split
+      if command_array[0] != 'place'
+        return false
+      else
+        if command_array.size <=1
+          return false
+        end
+        command_args = command_array[1].split(',')
+        if (Integer(command_args[0]) rescue nil) == nil
+          return false
+        elsif (Integer(command_args[1]) rescue nil) == nil
+          return false
+        elsif %w[north south east west].none?(command_args[2])
+          return false
+        else
+          return true
+        end
       end
     end
   end
-  def Left
 
+private
+  def can_move?(rows, columns)
+      case @direction.downcase
+      when 'north'
+          return true if columns.find_index(1) + 1 < columns.size
+      when 'south'
+          return true if columns.find_index(1) - 1 >= 0
+      when 'east'
+          return true if rows.find_index(1) + 1 < rows.size
+      else        #'west'
+          return true if (rows.find_index(1) - 1) >= 0
+      end
+    false
   end
-  def Right
 
+  def place(x, y, direction, rows, columns)
+    if x < rows.size && x >= 0 && y < columns.size && y >= 0
+      rows[x] = 1
+      columns[y] = 1
+      @direction = direction
+      @placed = true
+    else
+      puts "Out of board"
+    end
   end
-  def Report
 
+  def move(rows, columns)
+    if @placed
+      if can_move?(rows, columns)
+        if @direction == 'north'
+          index = columns.find_index(1)
+          columns[index] = 0
+          columns[index + 1] = 1
+        elsif @direction == 'south'
+          index = columns.find_index(1)
+          columns[index] = 0
+          columns[index - 1] = 1
+        elsif @direction == 'east'
+          index = rows.find_index(1)
+          rows[index] = 0
+          rows[index + 1] = 1
+        else            #west
+          index = rows.find_index(1)
+          rows[index] = 0
+          rows[index - 1] = 1
+        end
+      end
+    end
   end
-
+  def left
+    if @placed
+      if @direction == 'north'
+        @direction = 'west'
+      elsif @direction == 'south'
+        @direction = 'east'
+      elsif @direction == 'east'
+        @direction = 'north'
+      else            #west
+        @direction = 'south'
+      end
+    end
+  end
+  def right
+    if @placed
+      if @direction == 'north'
+        @direction = 'east'
+      elsif @direction == 'south'
+        @direction = 'west'
+      elsif @direction == 'east'
+        @direction = 'south'
+      else            #west
+        @direction = 'north'
+      end
+    end
+  end
+  def report(rows, columns)
+    if @placed
+      puts "x:#{rows.find_index(1)}, y:#{columns.find_index(1)}, direction:#{@direction}"
+    end
+  end
 end
